3cde5bb9082fd420651479d1e030a0544c1d91ae
Mohammadreza Ashouri, PhD <mohammad.r.ashouri@gmail.com>
2023-03-05 15:19:44 +0100

    Update MultiSigWallet.sol

--- MultiSigWallet.sol
+++ MultiSigWallet.sol
@@ -1,63 +1,178 @@
-// SPDX-License-Identifier: GPL-3.0
+// A secure multi-sig wallet written in Solidity
 
-pragma solidity ^0.8.9;
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.17;
 
+contract MoMultiSigWallet {
+    event Deposit(address indexed sender, uint amount, uint balance);
+    event SubmitTransaction(
+        address indexed owner,
+        uint indexed txIndex,
+        address indexed to,
+        uint value,
+        bytes data
+    );
+    event ConfirmTransaction(address indexed owner, uint indexed txIndex);
+    event RevokeConfirmation(address indexed owner, uint indexed txIndex);
+    event ExecuteTransaction(address indexed owner, uint indexed txIndex);
 
-contract MultiSigWallet{
+    address[] public owners;
+    mapping(address => bool) public isOwner;
+    uint public numConfirmationsRequired;
 
-//Events are used to inform external users that something happened on the blockchain
-event Deposit(address indexed sender, uint amount);
-address[] public walletowners;
-mapping(address => bool) public isOwner; //isOwner is used to make sure that the caller of the method is the owner of the contract
-//Mapping in Solidity acts like a hash table or dictionary in any other language
-//These are used to store the data in the form of key-value pairs
-
-uint public minimumrequired; // the minimum number if required approvals
-
-}
-
-struct Transaction{
-    address to;
-    uint value;
-    bytes date;
-    bool executed;
-}
-
-Transction[] public transactions; // we store all  the transactions in an array of Transaction
-
-mapping(uint=> mapping(address =>bool)) public approved; // we want to check whether the number of approvals are greater or equal than the minimum 
-//required to execute a transaction, so we store that in this mapping
-
-
-constractor(address[] memory _owners, uint required){ // _owners type is memory because we use it as the function parameter to store data temporary and we wont store it on the blockchian,
-
-    require(_owners.lenght >0, " no enough owners!"); // we need at least one owner
-    require(_required > 0 && _required <= _owners.length, "invalid number of owners!");// the required must be less than or equal to the number of owners
-    
-    
-    for(uint i; i < _owners.length; i++ ){
-        address owner = _owners[i];
-        require(owner != address(0), "invalid owner"); //  address(0) is a special address (burn address)
-
-        require(!isOwner[owner], "owner is no unique"); // isOwner is often used to make sure that the caller of the method is the owner of the contract
-
-        isOwner[owner] = true; 
-
-        owners.push(owner); // owner is a state variable, Every smart contract is owned by an address called as owner
-        //TIP: state variables whose values are permanently stored in a contract storage.
+    struct Transaction {
+        address to;
+        uint value;
+        bytes data;
+        bool executed;
+        uint numConfirmations;
     }
 
-    minimumrequired = required;
+    // mapping from tx index => owner => bool
+    mapping(uint => mapping(address => bool)) public isConfirmed;
+
+    Transaction[] public transactions;
+
+    modifier onlyOwner() {
+        require(isOwner[msg.sender], "not owner");
+        _;
+    }
+
+    modifier txExists(uint _txIndex) {
+        require(_txIndex < transactions.length, "tx does not exist");
+        _;
+    }
+
+    modifier notExecuted(uint _txIndex) {
+        require(!transactions[_txIndex].executed, "tx already executed");
+        _;
+    }
+
+    modifier notConfirmed(uint _txIndex) {
+        require(!isConfirmed[_txIndex][msg.sender], "tx already confirmed");
+        _;
+    }
+
+    constructor(address[] memory _owners, uint _numConfirmationsRequired) {
+        require(_owners.length > 0, "owners required");
+        require(
+            _numConfirmationsRequired > 0 &&
+                _numConfirmationsRequired <= _owners.length,
+            "invalid number of required confirmations"
+        );
+
+        for (uint i = 0; i < _owners.length; i++) {
+            address owner = _owners[i];
+
+            require(owner != address(0), "invalid owner");
+            require(!isOwner[owner], "owner not unique");
+
+            isOwner[owner] = true;
+            owners.push(owner);
+        }
+
+        numConfirmationsRequired = _numConfirmationsRequired;
+    }
+
+    receive() external payable {
+        emit Deposit(msg.sender, msg.value, address(this).balance);
+    }
+
+    function submitTransaction(
+        address _to,
+        uint _value,
+        bytes memory _data
+    ) public onlyOwner {
+        uint txIndex = transactions.length;
+
+        transactions.push(
+            Transaction({
+                to: _to,
+                value: _value,
+                data: _data,
+                executed: false,
+                numConfirmations: 0
+            })
+        );
+
+        emit SubmitTransaction(msg.sender, txIndex, _to, _value, _data);
+    }
+
+    function confirmTransaction(
+        uint _txIndex
+    ) public onlyOwner txExists(_txIndex) notExecuted(_txIndex) notConfirmed(_txIndex) {
+        Transaction storage transaction = transactions[_txIndex];
+        transaction.numConfirmations += 1;
+        isConfirmed[_txIndex][msg.sender] = true;
+
+        emit ConfirmTransaction(msg.sender, _txIndex);
+    }
+
+    function executeTransaction(
+        uint _txIndex
+    ) public onlyOwner txExists(_txIndex) notExecuted(_txIndex) {
+        Transaction storage transaction = transactions[_txIndex];
+
+        require(
+            transaction.numConfirmations >= numConfirmationsRequired,
+            "cannot execute tx"
+        );
+
+        transaction.executed = true;
+
+        (bool success, ) = transaction.to.call{value: transaction.value}(
+            transaction.data
+        );
+        require(success, "tx failed");
+
+        emit ExecuteTransaction(msg.sender, _txIndex);
+    }
+
+    function revokeConfirmation(
+        uint _txIndex
+    ) public onlyOwner txExists(_txIndex) notExecuted(_txIndex) {
+        Transaction storage transaction = transactions[_txIndex];
+
+        require(isConfirmed[_txIndex][msg.sender], "tx not confirmed");
+
+        transaction.numConfirmations -= 1;
+        isConfirmed[_txIndex][msg.sender] = false;
+
+        emit RevokeConfirmation(msg.sender, _txIndex);
+    }
+
+    function getOwners() public view returns (address[] memory) {
+        return owners;
+    }
+
+    function getTransactionCount() public view returns (uint) {
+        return transactions.length;
+    }
+
+    function getTransaction(
+        uint _txIndex
+    )
+        public
+        view
+        returns (
+            address to,
+            uint value,
+            bytes memory data,
+            bool executed,
+            uint numConfirmations
+        )
+    {
+        Transaction storage transaction = transactions[_txIndex];
+
+        return (
+            transaction.to,
+            transaction.value,
+            transaction.data,
+            transaction.executed,
+            transaction.numConfirmations
+        );
+    }
+
+
+
 }
-
-
-receive() external payable() { // other contracts can call this but internal functions cannot!
-
-    emit Deposit(msg.sender,msg.value);  //msg.sender special global variable to obtain the current user’s address (e.g., 0x44d33a….) from where the function call came
-}
-
-
-
-}
-
-
